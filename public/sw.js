fcdcadd7951a4f002d458b9d7071a412bf7250a3
webz3321-cmd <webz3321@gmail.com>
2026-06-20 11:19:37 +0530

    chore: migrate icon assets to PNG format
    Replace JPEG icons with PNGs to improve image quality and consistency, update the manifest and service worker accordingly, and add jimp for image processing utilities.

--- public/sw.js
+++ public/sw.js
@@ -5,8 +5,8 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.jpg',
-  '/icon-512.jpg'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
